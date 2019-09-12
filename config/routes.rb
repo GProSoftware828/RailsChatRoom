37b3a46d04b7c672f3d45ff4c29bb0a6eec4c9dc
GproSoftware828 <JetSendProductions@Gmail.com>
2019-09-12 13:09:45 +1000

    Added authentication controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
   get 'rooms/show'
   root to: 'rooms#show'
   mount ActionCable.server => '/cable'
